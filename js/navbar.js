04909b5c63cd84e3b4f73fcf3ea49f014c22da21
esacmens <95075661+esacmens@users.noreply.github.com>
2023-06-26 11:09:24 +0000

    commiting navbar and home

---
+++ js/navbar.js
@@ -0,0 +1,11 @@
+var hidemobil = document.querySelector(".mobile-style")
+
+
+function closemobile(){
+    //alert("j")
+    hidemobil.classList.toggle("hidmobilebater")
+}
+
+function mobilenav(){
+    hidemobil.classList.toggle("hidmobilebater")
+}
